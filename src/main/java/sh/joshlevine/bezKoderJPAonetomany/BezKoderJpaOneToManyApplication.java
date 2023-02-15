c9ea5d92c98bb8fe67efa172e275bf5c891f4422
Josh Levine <levine.joshuaadam@gmail.com>
2023-02-15 16:17:48 -0500

    initial setup

---
+++ src/main/java/sh/joshlevine/bezKoderJPAonetomany/BezKoderJpaOneToManyApplication.java
@@ -0,0 +1,13 @@
+package sh.joshlevine.bezKoderJPAonetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezKoderJpaOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezKoderJpaOneToManyApplication.class, args);
+	}
+
+}
